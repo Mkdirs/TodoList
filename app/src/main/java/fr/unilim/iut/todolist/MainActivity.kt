4adb8af300173a935336082df38efbf1f4af31d0
Ouidkling - Laptop <matteo.nadler.campou@gmail.com>
2023-02-28 10:37:28 +0100

    Ajout de la base du donnée du TP3

--- app/src/main/java/fr/unilim/iut/todolist/MainActivity.kt
+++ app/src/main/java/fr/unilim/iut/todolist/MainActivity.kt
@@ -1,11 +1,20 @@
 package fr.unilim.iut.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // DB Activity start
+        val intent: Intent = Intent(this, DBActivity::class.java)
+        val buttonDB: Button = findViewById(R.id.db_button)
+        buttonDB.setOnClickListener {
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/fr/unilim/iut/todolist/classes/EmpModelClass.kt
@@ -0,0 +1,8 @@
+package fr.unilim.iut.todolist.classes;
+
+//creating a Data Model Class
+class EmpModelClass (
+    var userId: Int,
+    val userName: String,
+    val userEmail: String
+)
